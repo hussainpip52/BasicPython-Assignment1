eba7ca610fb51acaa229c5febb85a2b63a31eb51
hussainpip52 <hussainpip52.gmail.com>
2025-09-25 00:40:50 +0530

    Program UpDATE

--- challange.py
+++ challange.py
@@ -15,4 +15,11 @@ print("Addition: ",addition)
 print("Subtraction: ",subtraction)
 print("Multiplication: ",multiplication)
 print("Division: ",division)
- 
+
+#Assignment 2
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+name=first_name+" "+last_name
+greeting="Hello, "+name+"!"+" Welcome to the python program."
+print(greeting)
